7506770e3aaf158f0385949bbc8dc434e5b44eeb
Damian Piętka <damianpietka@gmail.com>
2021-04-28 20:15:53 +0200

    installed styled-componends and created globalStyle

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import MainPage from './pages/MainPage';
 import OrderPage from './pages/OrderPage';
 import CartPage from './pages/CartPage';
 import NavigationBar from './components/NavigationBar';
+import GlobalStyle from './GlobalStyle';
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <NavigationBar />
       <Switch>
         <Route path='/' exact>
