a2c7d8a61257a6d092362d423516edb7add38be9
Damian Piętka <damianpietka@gmail.com>
2021-04-28 20:41:06 +0200

    basic navbar styling

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,23 +1,40 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const NavigationBar = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Strona Główna</Link>
-          </li>
-          <li>
-            <Link to='/zamow'>Zamow jedzenie</Link>
-          </li>
-          <li>
-            <Link to='/koszyk'>Koszyk</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <StyledHeader>
+      <div className='logo'>
+        <Link to='/'>Strona Główna</Link>
+      </div>
+
+      <div className='links'>
+        <Link to='/zamow'>Zamow jedzenie</Link>
+        <Link to='/koszyk'>Koszyk</Link>
+      </div>
+    </StyledHeader>
   );
 };
+
+const StyledHeader = styled.header`
+  background: #deb887;
+  min-height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem;
+  .logo {
+    flex: 3;
+  }
+  .links {
+    flex: 1;
+    display: flex;
+    justify-content: space-around;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
 
 export default NavigationBar;
